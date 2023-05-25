02fa66b0bd996cff829f46a029e4788d72855e10
DrabantBY <drabant2022@gmail.com>
2023-05-25 17:12:27 +0200

    add base config

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,13 @@
+import Logo from '../UI/Logo';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='container header__container'>
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Components/UI/Logo/index.tsx
@@ -0,0 +1,12 @@
+import { ReactComponent as LogoIcon } from '../../../assets/svg/logo.svg';
+
+export const Logo = () => {
+  return (
+    <a className='logo header__logo' href=''>
+      <LogoIcon />
+      louvre
+    </a>
+  );
+};
+
+export default Logo;
